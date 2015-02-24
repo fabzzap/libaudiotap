40273ba567b51271cf4a10a3c54240f6824ff32e
Fabrizio Gennari <fabrizio.ge@tiscali.it>
2015-02-24 13:47:39 +0100

    More readable code to load functions from DLL

--- libaudiotap_external_symbols.c
+++ libaudiotap_external_symbols.c
@@ -43,6 +43,18 @@ static enum library_status audiofile_init(){
   void *handle;
 #endif
 
+#if defined(WIN32)
+#define LOAD_FROM_LIBRARY(x,y) (void*)GetProcAddress(x,y)
+#else
+#define LOAD_FROM_LIBRARY(x,y) dlopen(x,y)
+#endif
+
+#define LOAD(x) \
+  x=LOAD_FROM_LIBRARY(handle, #x); \
+  if (!x) { \
+    return LIBRARY_SYMBOLS_MISSING; \
+  }
+
 static const char* audiofile_library_name =
 #if (defined _MSC_VER)
 #ifdef _DEBUG
@@ -72,167 +84,24 @@ static const char* audiofile_library_name =
     return LIBRARY_MISSING;
   }
 
-#if defined(WIN32)
-  afOpenFile = (void*)GetProcAddress(handle, "afOpenFile");
-#else
-  afOpenFile = dlsym(handle, "afOpenFile");
-#endif
-  if (!afOpenFile) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afCloseFile = (void*)GetProcAddress(handle, "afCloseFile");
-#else
-  afCloseFile = dlsym(handle, "afCloseFile");
-#endif
-  if (!afCloseFile) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afReadFrames = (void*)GetProcAddress(handle, "afReadFrames");
-#else
-  afReadFrames = dlsym(handle, "afReadFrames");
-#endif
-  if (!afReadFrames) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afWriteFrames = (void*)GetProcAddress(handle, "afWriteFrames");
-#else
-  afWriteFrames = dlsym(handle, "afWriteFrames");
-#endif
-  if (!afWriteFrames) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afSeekFrame = (void*)GetProcAddress(handle, "afSeekFrame");
-#else
-  afSeekFrame = dlsym(handle, "afSeekFrame");
-#endif
-  if (!afSeekFrame) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afSetVirtualChannels = (void*)GetProcAddress(handle, "afSetVirtualChannels");
-#else
-  afSetVirtualChannels = dlsym(handle, "afSetVirtualChannels");
-#endif
-  if (!afSetVirtualChannels) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afGetSampleFormat = (void*)GetProcAddress(handle, "afGetSampleFormat");
-#else
-  afGetSampleFormat = dlsym(handle, "afGetSampleFormat");
-#endif
-  if (!afGetSampleFormat) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afSetVirtualSampleFormat = (void*)GetProcAddress(handle, "afSetVirtualSampleFormat");
-#else
-  afSetVirtualSampleFormat = dlsym(handle, "afSetVirtualSampleFormat");
-#endif
-  if (!afSetVirtualSampleFormat) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afGetVirtualFrameSize = (void*)GetProcAddress(handle, "afGetVirtualFrameSize");
-#else
-  afGetVirtualFrameSize = dlsym(handle, "afGetVirtualFrameSize");
-#endif
-  if (!afGetVirtualFrameSize) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afGetFrameCount = (void*)GetProcAddress(handle, "afGetFrameCount");
-#else
-  afGetFrameCount = dlsym(handle, "afGetFrameCount");
-#endif
-  if (!afGetFrameCount) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afTellFrame = (void*)GetProcAddress(handle, "afTellFrame");
-#else
-  afTellFrame = dlsym(handle, "afTellFrame");
-#endif
-  if (!afTellFrame) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afInitFileFormat = (void*)GetProcAddress(handle, "afInitFileFormat");
-#else
-  afInitFileFormat = dlsym(handle, "afInitFileFormat");
-#endif
-  if (!afInitFileFormat) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afInitSampleFormat = (void*)GetProcAddress(handle, "afInitSampleFormat");
-#else
-  afInitSampleFormat = dlsym(handle, "afInitSampleFormat");
-#endif
-  if (!afInitSampleFormat) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afInitChannels = (void*)GetProcAddress(handle, "afInitChannels");
-#else
-  afInitChannels = dlsym(handle, "afInitChannels");
-#endif
-  if (!afInitChannels) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afInitRate = (void*)GetProcAddress(handle, "afInitRate");
-#else
-  afInitRate = dlsym(handle, "afInitRate");
-#endif
-  if (!afInitRate) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afGetRate = (void*)GetProcAddress(handle, "afGetRate");
-#else
-  afGetRate = dlsym(handle, "afGetRate");
-#endif
-  if (!afGetRate) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afNewFileSetup = (void*)GetProcAddress(handle, "afNewFileSetup");
-#else
-  afNewFileSetup = dlsym(handle, "afNewFileSetup");
-#endif
-  if (!afNewFileSetup) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  afFreeFileSetup = (void*)GetProcAddress(handle, "afFreeFileSetup");
-#else
-  afFreeFileSetup = dlsym(handle, "afFreeFileSetup");
-#endif
-  if (!afFreeFileSetup) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
+  LOAD(afOpenFile)
+  LOAD(afCloseFile)
+  LOAD(afReadFrames)
+  LOAD(afWriteFrames)
+  LOAD(afSeekFrame)
+  LOAD(afSetVirtualChannels)
+  LOAD(afGetSampleFormat)
+  LOAD(afSetVirtualSampleFormat)
+  LOAD(afGetVirtualFrameSize)
+  LOAD(afGetFrameCount)
+  LOAD(afTellFrame)
+  LOAD(afInitFileFormat)
+  LOAD(afInitSampleFormat)
+  LOAD(afInitChannels)
+  LOAD(afInitRate)
+  LOAD(afGetRate)
+  LOAD(afNewFileSetup)
+  LOAD(afFreeFileSetup)
 
   return LIBRARY_OK;
 }
@@ -264,78 +133,14 @@ static enum library_status portaudio_init(){
 #endif
   if (!handle)
     return LIBRARY_MISSING;
-
-#if defined(WIN32)
-  Pa_Initialize = (void*)GetProcAddress(handle, "Pa_Initialize");
-#else
-  Pa_Initialize = dlsym(handle, "Pa_Initialize");
-#endif
-   if (!Pa_Initialize) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  Pa_Terminate = (void*)GetProcAddress(handle, "Pa_Terminate");
-#else
-  Pa_Terminate = dlsym(handle, "Pa_Terminate");
-#endif
-   if (!Pa_Terminate) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-  Pa_OpenDefaultStream = (void*)GetProcAddress(handle, "Pa_OpenDefaultStream");
-#else
-  Pa_OpenDefaultStream = dlsym(handle, "Pa_OpenDefaultStream");
-#endif
-   if (!Pa_OpenDefaultStream) {
-    return LIBRARY_SYMBOLS_MISSING;
-  }
-
-#if defined(WIN32)
-   Pa_CloseStream = (void*)GetProcAddress(handle, "Pa_CloseStream");
-#else
-   Pa_CloseStream = dlsym(handle, "Pa_CloseStream");
-#endif
-   if (!Pa_CloseStream) {
-     return LIBRARY_SYMBOLS_MISSING;
-   }
-
-#if defined(WIN32)
-   Pa_StartStream = (void*)GetProcAddress(handle, "Pa_StartStream");
-#else
-   Pa_StartStream = dlsym(handle, "Pa_StartStream");
-#endif
-   if (!Pa_StartStream) {
-     return LIBRARY_SYMBOLS_MISSING;
-   }
-
-#if defined(WIN32)
-   Pa_StopStream = (void*)GetProcAddress(handle, "Pa_StopStream");
-#else
-   Pa_StopStream = dlsym(handle, "Pa_StopStream");
-#endif
-   if (!Pa_StopStream) {
-     return LIBRARY_SYMBOLS_MISSING;
-   }
-
-#if defined(WIN32)
-   Pa_ReadStream = (void*)GetProcAddress(handle, "Pa_ReadStream");
-#else
-   Pa_ReadStream = dlsym(handle, "Pa_ReadStream");
-#endif
-   if (!Pa_ReadStream) {
-     return LIBRARY_SYMBOLS_MISSING;
-   }
-
-#if defined(WIN32)
-   Pa_WriteStream = (void*)GetProcAddress(handle, "Pa_WriteStream");
-#else
-   Pa_WriteStream = dlsym(handle, "Pa_WriteStream");
-#endif
-   if (!Pa_WriteStream) {
-     return LIBRARY_SYMBOLS_MISSING;
-   }
+  LOAD(Pa_Initialize)
+  LOAD(Pa_Terminate)
+  LOAD(Pa_OpenDefaultStream)
+  LOAD(Pa_CloseStream)
+  LOAD(Pa_StartStream)
+  LOAD(Pa_StopStream)
+  LOAD(Pa_ReadStream)
+  LOAD(Pa_WriteStream)
 
   if (Pa_Initialize() != paNoError)
   {
@@ -370,68 +175,13 @@ static enum library_status libtapencoder_init(){
   if (!handle)
     return LIBRARY_MISSING;
 
-  tapenc_init2 = 
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapenc_init2");
-#else
-  dlsym(handle, "tapenc_init2");
-#endif
-  if (!tapenc_init2)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapenc_exit = 
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapenc_exit");
-#else
-  dlsym(handle, "tapenc_exit");
-#endif
-  if (!tapenc_exit)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapenc_get_pulse =
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapenc_get_pulse");
-#else
-  dlsym(handle, "tapenc_get_pulse");
-#endif
-  if (!tapenc_get_pulse)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapenc_flush =
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapenc_flush");
-#else
-  dlsym(handle, "tapenc_flush");
-#endif
-  if (!tapenc_flush)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapenc_get_max =
-#if defined(WIN32)
-   (void*)GetProcAddress(handle, "tapenc_get_max");
-#else
-   dlsym(handle, "tapenc_get_max");
-#endif
-   if (!tapenc_get_max)
-     return LIBRARY_SYMBOLS_MISSING;
-
-  tapenc_invert =
-#if defined(WIN32)
-   (void*)GetProcAddress(handle, "tapenc_invert");
-#else
-   dlsym(handle, "tapenc_invert");
-#endif
-   if (!tapenc_invert)
-     return LIBRARY_SYMBOLS_MISSING;
-
-  tapenc_toggle_trigger_on_both_edges =
-#if defined(WIN32)
-   (void*)GetProcAddress(handle, "tapenc_toggle_trigger_on_both_edges");
-#else
-   dlsym(handle, "tapenc_toggle_trigger_on_both_edges");
-#endif
-   if (!tapenc_toggle_trigger_on_both_edges)
-     return LIBRARY_SYMBOLS_MISSING;
+  LOAD(tapenc_init2)
+  LOAD(tapenc_exit)
+  LOAD(tapenc_get_pulse)
+  LOAD(tapenc_flush)
+  LOAD(tapenc_get_max)
+  LOAD(tapenc_invert)
+  LOAD(tapenc_toggle_trigger_on_both_edges)
 
   return LIBRARY_OK;
 }
@@ -462,51 +212,12 @@ static enum library_status libtapdecoder_init(){
   if (!handle)
     return LIBRARY_MISSING;
 
-  tapdec_init2 = 
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapdec_init2");
-#else
-  dlsym(handle, "tapdec_init2");
-#endif
-  if (!tapdec_init2)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapdec_exit = 
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapdec_exit");
-#else
-  dlsym(handle, "tapdec_exit");
-#endif
-  if (!tapdec_exit)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapdec_set_pulse =
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapdec_set_pulse");
-#else
-
-  dlsym(handle, "tapdec_set_pulse");
-#endif
-  if (!tapdec_set_pulse)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapdec_get_buffer =
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapdec_get_buffer");
-#else
-  dlsym(handle, "tapdec_get_buffer");
-#endif
-  if (!tapdec_get_buffer)
-    return LIBRARY_SYMBOLS_MISSING;
-
-  tapdec_enable_halfwaves =
-#if defined(WIN32)
-  (void*)GetProcAddress(handle, "tapdec_enable_halfwaves");
-#else
-  dlsym(handle, "tapdec_enable_halfwaves");
-#endif
-  if (!tapdec_enable_halfwaves)
-    return LIBRARY_SYMBOLS_MISSING;
+  LOAD(tapdec_init2)
+  LOAD(tapdec_exit)
+  LOAD(tapdec_set_pulse)
+  LOAD(tapdec_get_buffer)
+  LOAD(tapdec_enable_halfwaves)
+  LOAD(tapdec_enable_halfwaves)
 
   return LIBRARY_OK;
 }
